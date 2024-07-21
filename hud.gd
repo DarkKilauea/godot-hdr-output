6d5026fed28f167a97735f12a620a2e5273be859
Josh Jones <kilauea.jones@gmail.com>
2024-07-20 21:19:39 -0700

    Added more controls

--- hud.gd
+++ hud.gd
@@ -1,22 +1,38 @@
 extends Control
 
 
-@onready var enable_hdr_button: CheckButton = %EnableHDRButton
-@onready var max_luminance_value: Label = %MaxLuminanceValue
-@onready var max_luminance_slider: HSlider = %MaxLuminanceSlider
+@onready var _enable_hdr_button: CheckButton = %EnableHDRButton
+@onready var _3d_max_luminance_slider: SliderControl = %"3dMaxLuminanceSlider"
+@onready var _2d_max_luminance_slider: SliderControl = %"2dMaxLuminanceSlider"
+@onready var _brightness_slider_control: SliderControl = %BrightnessSliderControl
+@onready var _contrast_slider_control: SliderControl = %ContrastSliderControl
+@onready var _world_environment: WorldEnvironment = %WorldEnvironment
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	enable_hdr_button.set_pressed_no_signal(DisplayServer.window_get_hdr_output_enabled());
-	max_luminance_slider.set_value_no_signal(DisplayServer.window_get_hdr_output_max_luminance());
-	max_luminance_value.text = "%d" % max_luminance_slider.value;
+	_enable_hdr_button.set_pressed_no_signal(DisplayServer.window_get_hdr_output_enabled());
+	_3d_max_luminance_slider.value = DisplayServer.window_get_hdr_output_max_luminance();
+	# _2d_max_luminance_slider.value = ;
+	_brightness_slider_control.value = _world_environment.environment.adjustment_brightness;
+	_contrast_slider_control.value = _world_environment.environment.adjustment_contrast;
 
 
 func _on_enable_hdr_button_toggled(toggled_on: bool) -> void:
 	DisplayServer.window_set_hdr_output_enabled(toggled_on);
 
 
-func _on_max_luminance_slider_value_changed(value: float) -> void:
+func _on_3d_max_luminance_slider_value_changed(value: float) -> void:
 	DisplayServer.window_set_hdr_output_max_luminance(value);
-	max_luminance_value.text = "%d" % value;
+
+
+func _on_2d_max_luminance_slider_value_changed(value: float) -> void:
+	pass
+
+
+func _on_brightness_slider_control_value_changed(value: float) -> void:
+	_world_environment.environment.adjustment_brightness = value;
+
+
+func _on_contrast_slider_control_value_changed(value: float) -> void:
+	_world_environment.environment.adjustment_contrast = value;
---
+++ slider_control.gd
@@ -0,0 +1,96 @@
+@tool
+class_name SliderControl
+extends Control
+
+
+signal value_changed(value);
+
+
+@export var min_value: float:
+	set(v):
+		if is_equal_approx(_min_value, v):
+			return;
+		
+		_min_value = v;
+		
+		if value_slider:
+			value_slider.min_value = v;
+	get():
+		return _min_value;
+
+@export var max_value: float:
+	set(v):
+		if is_equal_approx(_max_value, v):
+			return;
+		
+		_max_value = v;
+		
+		if value_slider:
+			value_slider.max_value = v;
+	get():
+		return _max_value;
+
+@export var step: float = 1.0:
+	set(v):
+		if is_equal_approx(_step, v):
+			return;
+		
+		_step = v;
+		
+		if value_slider:
+			value_slider.step = v;
+	get():
+		return _step;
+
+@export var title: String:
+	set(v):
+		if _title == v:
+			return;
+		
+		_title = v;
+		
+		if title_label:
+			title_label.text = _title;
+	get():
+		return _title;
+
+@export var value: float:
+	set(v):
+		if is_equal_approx(_value, v):
+			return;
+		
+		_value = v;
+		if value_label:
+			value_label.text = "%f" % v;
+		if value_slider:
+			value_slider.set_value_no_signal(v);
+	get():
+		return _value;
+
+
+@onready var title_label: Label = $HBoxContainer/TitleLabel
+@onready var value_label: Label = $HBoxContainer/ValueLabel
+@onready var value_slider: HSlider = $ValueSlider
+
+
+var _min_value: float;
+var _max_value: float;
+var _step: float;
+var _title: String;
+var _value: float;
+
+
+func _ready() -> void:
+	title_label.text = _title;
+	value_label.text = "%.2f" % _value;
+	value_slider.max_value = _max_value;
+	value_slider.min_value = _min_value;
+	value_slider.step = _step;
+	value_slider.set_value_no_signal(_value);
+
+
+func _on_value_slider_value_changed(v: float) -> void:
+	_value = v;
+	value_label.text = "%f" % v;
+	
+	value_changed.emit(v);
